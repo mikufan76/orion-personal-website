246e90e59978c8b5cbf38bfcace66211dde9dff2
mikufan76 <tnd3015@rit.edu>
2024-05-17 09:26:40 -0400

    site restructured, mila with spinner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css';
+import '@radix-ui/themes/styles.css';
 import { inter } from '@/app/ui/fonts';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
 export default function RootLayout({
   children,
@@ -8,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}><Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">{children}</Theme></body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,38 @@
+'use client';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { Flex, Text, Button, Spinner } from '@radix-ui/themes';
+import { useState } from 'react';
 
 export default function Page() {
+  const [loading, setLoading] = useState(false);
+  function handleClick() {
+    setLoading(!loading);
+  }
+
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
-          <p
-            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/milasmall.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Mila wearing an outfit. Her ears are covered. Absolute unit."
-          />
-          <Image className="block md:hidden" src='/milasmile.png' width={560} height={620} alt='Mila smiling'/>
-        </div>
-      </div>
+      <Flex direction="column" gap="2" justify={'center'} align={'center'}>
+        <Spinner loading={loading} size="3" />
+        <Image
+          src="/milasmall.png"
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="Mila wearing an outfit. Her ears are covered. Absolute unit."
+        />
+        <Image
+          className="block md:hidden"
+          src="/milasmile.png"
+          width={560}
+          height={620}
+          alt="Mila smiling"
+        />
+        <Button onClick={handleClick}>Let's go</Button>
+      </Flex>
     </main>
   );
 }
