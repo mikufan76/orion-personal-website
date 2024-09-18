8f58abbed781a7e80b462e126b02126e1e055878
mikufan76 <56976587+mikufan76@users.noreply.github.com>
2024-09-18 12:08:08 -0400

    remove react

--- app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Lusitana } from 'next/font/google';
-export const inter = Inter({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-inter',
-});
-
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
